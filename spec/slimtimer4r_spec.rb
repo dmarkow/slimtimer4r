05a7393319d882783b64c81ce5b6a38385292f5e
Dylan Markow <dm@petefowler.com>
2011-02-02 16:48:13 -0800

    Begin restructuring

--- spec/slimtimer4r_spec.rb
+++ spec/slimtimer4r_spec.rb
@@ -9,13 +9,13 @@ describe SlimTimer do
     access_token: cea67ae2c4b7fa3be496f508f52aad6230b2684a
     EOF
     stub_request(:post, "http://slimtimer.com/users/token").to_return(:body => response_data)
-    @st = SlimTimer.new("good@example.com","secret","12345")
+    @st = SlimTimer::Client.new("good@example.com","secret","12345")
     @st.access_token.should_not == nil
   end
   
   it "fails to initialize with improper login info" do
     stub_request(:post, "http://slimtimer.com/users/token").to_return(:status => 500)
-    lambda { @st = SlimTimer.new("bad@example.com","secret","12345") }.should raise_error
+    lambda { @st = SlimTimer::Client.new("bad@example.com","secret","12345") }.should raise_error
   end
 
 end
